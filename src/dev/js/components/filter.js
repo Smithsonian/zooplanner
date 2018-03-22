8adc4d4f5756eff8918e8e9a980294379367b7e8
Irene Li <irene@Irenes-MacBook-Air.local>
2018-03-22 04:31:01 -0700

    EDIT: changed css of filter so it does not wrap to newline

--- src/dev/js/components/filter.js
+++ src/dev/js/components/filter.js
@@ -14,18 +14,16 @@ class Filter extends Component {
 							<input type='checkbox' id='ammenities' name='filter' value='ammenities' onClick={this.props.updateCheckbox} />
 							<label htmlFor='ammenities'>AMMENITIES</label>
 						</div>
-						<div className="col-6" id='filterlabel'>
-							<input type='checkbox' id='exhibits' name='filter' value='exhibits' onClick={this.props.updateCheckbox} />
-							<label htmlFor='exhibits'>EXHIBITS</label>
+						<div className="col-5" id='filterlabel'>
+							<label><input type='checkbox' id='exhibits' name='filter' value='exhibits' onClick={this.props.updateCheckbox} />&nbsp;&nbsp;&nbsp;&nbsp;EXHIBITS <img src="https://www.google.com.au/maps/vt/icon/name=assets/icons/spotlight/spotlight_pin_v2_shadow-1-small.png,assets/icons/spotlight/spotlight_pin_v2-1-small.png,assets/icons/spotlight/spotlight_pin_v2_dot-1-small.png,assets/icons/spotlight/spotlight_pin_v2_accent-1-small.png&highlight=93FF95,C34D57,960a0a,ffffff&color=93FF95?scale=0.5"/></label>
 						</div>
 					</div>
 
 					<div className="row">
 						<div className="col-6" id='filterlabel'>
-							<input type='checkbox' id='attractions' name='filter' value='attractions' onClick={this.props.updateCheckbox} />
-							<label htmlFor='attractions'>ATTRACTIONS</label>
+							<label><input type='checkbox' id='attractions' name='filter' value='attractions' onClick={this.props.updateCheckbox} />&nbsp;&nbsp;&nbsp;&nbsp;ATTRACTIONS <img src="https://www.google.com.au/maps/vt/icon/name=assets/icons/spotlight/spotlight_pin_v2_shadow-1-small.png,assets/icons/spotlight/spotlight_pin_v2-1-small.png,assets/icons/spotlight/spotlight_pin_v2_dot-1-small.png,assets/icons/spotlight/spotlight_pin_v2_accent-1-small.png&highlight=93FF95,F18B30,960a0a,ffffff&color=93FF95?scale=0.5"/></label>	
 						</div>
-						<div className="col-6" id='filterlabel'>
+						<div className="col-5" id='filterlabel'>
 							<input type='checkbox' id='food' name='filter' value='food' onClick={this.props.updateCheckbox} />
 							<label htmlFor='food'>FOOD</label>
 						</div>
@@ -36,7 +34,7 @@ class Filter extends Component {
 							<input type='checkbox' id='dailyprogs' name='filter' value='dailyprogs' onClick={this.props.updateCheckbox} />
 							<label htmlFor='dailyprogs'>DAILY&nbsp;PROGRAMS</label>
 						</div>
-						<div className="col-6" id='filterlabel'>
+						<div className="col-5" id='filterlabel'>
 							<input type='checkbox' id='restrooms' name='filter' value='restrooms' onClick={this.props.updateCheckbox} />
 							<label htmlFor='restrooms'>RESTROOMS</label>
 						</div>
