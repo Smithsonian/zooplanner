15a0f330a673f71627c49059533d6da1790d3230
Irene Li <irene@Irenes-MacBook-Air.local>
2018-03-23 04:15:48 -0700

    ADD: added feature of dragging the trip elements to reorder them

--- src/dev/js/components/trip.js
+++ src/dev/js/components/trip.js
@@ -3,7 +3,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import '../../css/Main.css';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux'
-import {removeFromTrip} from '../actions/tripActions'
+import {removeFromTrip, updateTrip} from '../actions/tripActions'
+import Reorder, { reorder, reorderImmutable, reorderFromTo, reorderFromToImmutable } from 'react-reorder';
 
 class Trip extends Component {
 
@@ -16,6 +17,12 @@ class Trip extends Component {
     removeFromTrip(item) {
         this.props.removeFromTrip(item);
     }
+
+    onReorder (event, previousIndex, nextIndex, fromId, toId) {
+        const newTrip = reorder(this.props.trip, previousIndex, nextIndex);
+        this.props.updateTrip(newTrip);
+        console.log(this.props.trip);
+    }
     
     render() {
         var listItem = <p></p>
@@ -26,7 +33,7 @@ class Trip extends Component {
                     <p id='emptyTripText'>You have not added anything yet <br/><br/><br/> Need help planning?<br/> See pre-planned trips</p>
                 </div>
             );   
-        } else {
+        } else if (this.props.trip.length < 2) {
             let passedList = this.convertToArray(this.props.trip);
             listItem = passedList.map((item) => {
                 item = item[1];
@@ -46,12 +53,41 @@ class Trip extends Component {
                     </div>
                 )
             });
+        } else {
+            let passedList = this.convertToArray(this.props.trip);
+            listItem = <Reorder
+                            reorderId="my-list" 
+                            placeholderClassName="placeholder"
+                            draggedClassName="dragged"
+                            lock="horizontal"
+                            onReorder={this.onReorder.bind(this)} // Callback when an item is dropped (you will need this to update your state)
+                            autoScroll={true}
+                        >
+                            
+                            {passedList.map((item) => {
+                                item = item[1];
+                                return (
+                                    <div className='simpleItem' key={item.title}>
+                                        <div className="row">
+                                            <div className="col-3" id="itemImage">
+                                                <a href="#" alt={item.title}><img alt={item.title} src={item.image}/></a>
+                                            </div>
+                                            <div className='col-8' id="itemInfo">
+                                                <button type="button" title='REMOVE' className="btn btn-remove" onClick={() => this.removeFromTrip(item)}><i className="glyphicon glyphicon-remove"></i></button>
+                                                <a id='itemName' alt={item.title} href="#" title={item.title}>{item.title}</a>
+                                                <br/>
+                                                <p id="itemLocation">{item.exhibit_name}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            })}
+                        </Reorder>
         }
         return (
             <div id="tripList">
                 {listItem}
-            </div>
-            
+            </div>   
         )
     }
 }
@@ -64,7 +100,8 @@ function mapStateToProps(state) {
 function matchDispatchToProps(dispatch) {
 	return bindActionCreators({
         // expandAnimal: expandAnimal,
-        removeFromTrip: removeFromTrip
+        removeFromTrip: removeFromTrip,
+        updateTrip: updateTrip,
 	}, dispatch);
 }
 export default connect(mapStateToProps, matchDispatchToProps)(Trip);
