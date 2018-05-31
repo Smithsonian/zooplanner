e11348f0ba8fe49050b1a1b991477cdd6e0e07d9
Irene Li <ireneaili@berkeley.edu>
2018-05-31 01:49:42 -0700

    EDIT: added map to printing. BUG: CSS is not preserved and looks funky when we actually go to print

--- src/dev/js/components/document.js
+++ src/dev/js/components/document.js
@@ -3,11 +3,62 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {setPage} from "../actions/AppActions"
 import MyMapComponent from "./map.js"
+import Trip from "./trip.js"
 
 
 class MyDocument extends Component {
+    convertToArray(object) {
+		var result = Object.keys(object).map(function(key) {
+			return [Number(key), object[key]];
+		});
+		return result;
+    }
 
     render() {
+        let passedList = this.convertToArray(this.props.trip);
+        var listItem = passedList.map((item) => {
+            item = item[1];
+            var itemLocation;
+            if (item.exhibit_name == null) {
+                itemLocation = <p id="itemLocation"></p>
+            } else {
+                itemLocation = <p id="itemLocation">{item.exhibit_name.replace(/&#039;/g, "'")}</p>
+            }
+            var image;
+            switch (item.type) { //to change image variable
+                case "Exhibit": {
+                    image = item.image_small;
+                    break;
+                } case "Attraction": {
+                    image = item.image_small;
+                    break;
+                } case "Animal": {
+                    image = item.image_small;
+                    break;
+                } case "Restroom": {
+                    image = item.image;
+                    break;
+                } case undefined: {
+                    image = "https://farm1.staticflickr.com/956/27940952588_0296bce9a3_m.jpg"
+                    break;
+                } default: break;
+            }        
+            return (
+                <div className='simpleItem' key={item.title}>
+                    <div className="row">
+                        <div className="col-3" id="itemImage">
+                            <a href={window.location.hash} alt={item.title}><img alt={item.title} src={image}/></a>
+                        </div>
+                        <div className='col-8' id="itemInfo">
+                            <a id='itemName' alt={item.title} href={window.location.hash} title={item.title}>{item.title.replace(/&#039;/g, "'")}</a>
+                            <br/>
+                            {itemLocation}
+                        </div>
+                    </div>
+                </div>
+            )
+        });
+
         return (
             <div> 
                 <div className="row" id="printTopBar">
@@ -18,8 +69,14 @@ class MyDocument extends Component {
                         <button type='submit' className='btn btn-default' id='printButton' onClick={() => window.print()}>PRINT</button>
                     </div>
                 </div>
-                <MyMapComponent/>
                 <div id="divname">
+                    <p id='zooplanner'> YOUR TRIP </p>
+                    <div id="printMapContainer">
+                        <MyMapComponent/>
+                    </div>
+                    <div id="printListItem">
+                        {listItem}
+                    </div>
 					
                 </div>
             </div>
